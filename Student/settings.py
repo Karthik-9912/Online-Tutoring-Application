ec7d08a8eb31f3c08becbe8752f2071c0a9a03db
B Karthik <167326683+Karthik-9912@users.noreply.github.com>
2025-09-07 11:51:44 +0530

    Update settings.py

--- Student/settings.py
+++ Student/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mb958@9*g5yi=02ct0j0t99%ep59ri2^3lu2@tompunx77g6+l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
